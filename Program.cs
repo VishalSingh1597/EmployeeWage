2f1d56c55fc24c6f0286296c3a7cb071d7207c27
Vishal Singh <VishalSingh1597>
2021-04-15 21:07:29 +0530

    Added Code of PArt Time Employee

--- Program.cs
+++ Program.cs
@@ -10,29 +10,41 @@ namespace EmployeeWage
     { 
         static void Main(string[] args)
         {
-                //Constraits
-                int Present = 1;
-                int EmpRatePerHours = 20;
+                //Constant Variable
+                int FullTime = 1;                                        //creating a local variable
+                int PartTime = 2;
+                int empRatePerHour = 20;
+
                 //Local Variables
                 int empHrs = 0;
-                int empwage = 0;
+                int empWage = 0;
 
-                //inbuilt class
-                Random randomvalue = new Random();
-                int employeCheck = randomvalue.Next(0, 2);
-                Console.WriteLine("random value:-" + employeCheck);
-                if (employeCheck == Present)
+                Random random = new Random();                           //Generating Random value
+                int employeeCheck = random.Next(0, 3);                  // assigning Random value to the variable 
+                Console.WriteLine("Random Value: " + employeeCheck);
+
+
+                if (employeeCheck == FullTime)                          //Checking whether Employee is Present or Not
                 {
+                    Console.WriteLine("Employee is PRESENT");
                     empHrs = 8;
-
+                }
+                else if (employeeCheck == PartTime)
+                {
+                    Console.WriteLine("Part Time Empoyee: ");
+                    empHrs = 4;
                 }
                 else
                 {
+                    Console.WriteLine("Employess is ABSENT");
                     empHrs = 0;
                 }
-                empwage = EmpRatePerHours * empHrs;
-                Console.WriteLine("Empolyee wage perhours:" + empHrs);
+
+                //computation
+                empWage = empRatePerHour * empHrs;
+                Console.WriteLine("Employee wage per day: " + empWage);
                 Console.Read();
             }
+
         }
     }
