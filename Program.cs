ec1a296bc787de96c976a32ecefc1f5178db2b82
Vishal Singh <VishalSingh1597>
2021-04-18 17:14:19 +0530

    Added List For MultipleCompany Wage

--- Program.cs
+++ Program.cs
@@ -11,13 +11,11 @@ namespace EmpWageComputation
 		static void Main(string[] args)
 		{
 			Console.WriteLine("welcome to employee Program");
-			EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
-			empWageBuilder.addCompanyEmpWage("DMart", 20, 2, 10);
-			empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
-			CompanyEmpWage dmart = new CompanyEmpWage("DMart", 20, 2, 10);
-			CompanyEmpWage reliance = new CompanyEmpWage("Reliance", 10, 4, 20);
-			double dmartPrice = empWageBuilder.computeEmpWage(dmart);
-			double reliancePrice = empWageBuilder.computeEmpWage(reliance);
+			EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();  //creat object of EmpWageBuilderArray class
+			empWageBuilder.AddCompanyEmpWage("DMart", 20, 2, 10); //call AddCompany method
+			empWageBuilder.AddCompanyEmpWage("Reliance", 10, 4, 20);
+			empWageBuilder.computeEmpWage();
+			Console.WriteLine("Total Wage for DMart Comapny " + empWageBuilder.getTotalWage("DMart"));
 			Console.Read();
 		}
 
